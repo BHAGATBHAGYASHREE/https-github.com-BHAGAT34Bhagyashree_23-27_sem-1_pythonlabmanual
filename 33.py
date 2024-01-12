199b221a96882d97949fe0af00ab25381ecb882b
Bhagyashree Bhagat <142775493+BHAGATBHAGYASHREE@users.noreply.github.com>
2024-01-12 13:17:55 +0530

    Delete 33.py

--- 33.py
+++
@@ -1,8 +0,0 @@
-def get_distinct_elements(input_list):
-    return list(set(input_list))
-
-user_input = input("Enter elements separated by spaces: ")
-input_list = list(map(int, user_input.split()))
-
-result_list = get_distinct_elements(input_list)
-print(result_list)
